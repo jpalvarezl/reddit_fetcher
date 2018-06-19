3986ee1d77984507179e7f039a0cd4a9e1cf4ef2
Jose Alvarez <jp.alvarezl@gmail.com>
2018-06-19 12:09:07 +0200

    Cleaned up comments from mix.exs

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule RedditFetcher.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
+
   def application do
     [
       extra_applications: [:logger]
@@ -23,11 +23,7 @@ defmodule RedditFetcher.MixProject do
     [main_module: RedditFetcher.CLI]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-    ]
+    []
   end
 end
