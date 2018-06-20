d4c7858fab0923c164ab6037421f50bac79c54a1
Jose Alvarez <jp.alvarezl@gmail.com>
2018-06-20 15:14:18 +0100

    Version with local file formats reddits as expected

--- lib/reddit_fetcher.ex
+++ lib/reddit_fetcher.ex
@@ -15,18 +15,29 @@ defmodule RedditFetcher.CLI do
   end
 
   defp response({opts, word}) do
-    if opts[:verbose], do: "VERBOSE MODE IS ON BITCH: #{word}", else: word
+    if opts[:verbose], do: "VERBOSE MODE IS ON: #{word}", else: word
     fetch_comments()
   end
 
   defp fetch_comments() do
-    {:ok, response} = HTTPoison.get("https://www.reddit.com/r/programming.json")
+    # {:ok, response} = HTTPoison.get("https://www.reddit.com/r/programming.json")
     # File.write!("./programming.json", response.body)
-    response.body
+    # response.body
+    {:ok, file} = File.read("./programming.json")
+    file
     |> Poison.decode!
     |> Map.get("data")
     |> Map.get("children")
+    |> Enum.map(&format_reddit(&1))
     |> IO.inspect
+  end
+
+  defp format_reddit(reddit) do
+    data = Map.get(reddit, "data")
+    %{
+      title: Map.get(data, "title"),
+      permalink: "https://reddit.com#{Map.get(data, "permalink")}"
+    }
   end
 
 end
